c5252e890d980de425c0c7ef738c76a9bbbac1d1
Danielwhyte <danwhy@gmail.com>
2015-05-27 17:28:02 +0100

    trying to get tests working in iframe

---
+++ script.js
@@ -0,0 +1,51 @@
+var ClientSide = (function(){
+
+var suggestions;
+
+$('#search').keyup(function(e){
+  var word = $('#search').val();
+  if (word.length > 2){
+    $.get('/find/'+word, function handler(data) {
+      var words = data.split(',');
+      var results = '';
+      words.forEach(function(w) {
+        results += "<div class='suggestion'><p class='word'> " + w + "</p></div>";
+      });
+      $('#results').html(results);
+      suggestionUpdater();
+    });
+  }
+});
+
+
+function suggestionUpdater(){
+    suggestions = [].slice.call(document.getElementsByClassName('suggestion'));
+    suggestions.forEach(function(element){
+        element.addEventListener('click', ClientSide.getDefinition);
+    });
+}
+
+function getDefinition(){
+    var definition;
+    if (this.children.length > 1){
+        this.removeChild(this.lastChild)
+    } else {
+        var word = this.getElementsByClassName('word')[0].innerHTML;
+        var request = new XMLHttpRequest;
+        request.open('GET', '/define/' + word);
+        request.onreadystatechange = function(){
+            if (request.readyState === 4){
+                if (request.status === 200){
+                    definition = request.responseText;
+                }
+            }
+        }
+        this.innerHTML += '<p>' + definition + '</p>';
+    }
+}
+
+return {
+    getDefinition: getDefinition
+}
+
+}());
